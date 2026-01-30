020129320e7b083b7ad0e1b29fde1fc786039184
tokh7ash <gelether123@gmail.com>
2026-01-30 14:17:35 +0500

    haha

--- practice1/task1.py
+++ practice1/task1.py
@@ -1,9 +1,2 @@
-#python coments!
-# x = 5
-
-# y = 6
-# z= 10
-name = input('enter your name:',)
-print ("Hello, ",name)
-age = input(f"enter your age {name}:",)
-print(name, f"{age} years old")
+a = input()
+print(a[0],a[-1])
